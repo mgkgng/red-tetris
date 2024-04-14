63bb17477d321cf3a0746aeda5848fc5dc2f9812
Julien Caucheteux <jdaodkql@gmail.com>
2024-04-14 22:53:16 +0200

    tests: add Player unit tests

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import http from 'http';
-import { initSocketServer } from './socket.js';
-import apiRoutes from './routes/api.js'; // Import your API routes
-
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { initSocketServer } from "./socket.js";
+import apiRoutes from "./routes/api.js"; // Import your API routes
 
 const PORT = 3000;
 const app = express();
@@ -13,10 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 // Use your routes
-app.use('/api', apiRoutes); // This mounts your API routes under the /api prefix
+app.use("/api", apiRoutes); // This mounts your API routes under the /api prefix
 
 initSocketServer(server);
 
 server.listen(3000, () => {
-    console.log(`Red-tetris server initialized on port ${PORT}...`);
+  console.log(`Red-tetris server initialized on port ${PORT}...`);
 });
