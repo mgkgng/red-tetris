0670241a09370da25fc5e60f1e0d2f8e124f637f
Julien Caucheteux <jdaodkql@gmail.com>
2024-04-14 22:46:10 +0200

    feat: improve jest config

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,6 +2,16 @@ module.exports = {
 	transform: {
 	  '^.+\\.js$': 'babel-jest'
 	},
-	testEnvironment: 'node'
-  };
+	testEnvironment: 'node',
+	collectCoverage: true, // This line enables coverage collection
+	coverageThreshold: {  // Define minimum coverage thresholds
+	  global: {
+		statements: 70,
+		branches: 50,
+		functions: 70,
+		lines: 70
+	  }
+	},
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
   
