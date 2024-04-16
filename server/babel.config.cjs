260d9f3e4f1716cce8721d2ff01c899c8852ad8a
Julien Caucheteux <jdaodkql@gmail.com>
2024-04-16 14:50:50 +0200

    fix: add back module type in package.json, adapt unit tests

--- server/babel.config.cjs
+++ server/babel.config.cjs
@@ -4,4 +4,3 @@ module.exports = {
 	],
 	plugins: ["transform-import-meta"]
   };
-  
--- server/jest.config.js
+++ server/jest.config.js
@@ -1,17 +1,19 @@
-module.exports = {
-	transform: {
-	  '^.+\\.js$': 'babel-jest'
-	},
-	testEnvironment: 'node',
-	collectCoverage: true,
-	coverageThreshold: {
-	  global: {
-		statements: 70,
-		branches: 50,
-		functions: 70,
-		lines: 70
-	  }
-	},
-	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+/** @type {import('jest').Config} */
+const config = {
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      branches: 50,
+      functions: 70,
+      lines: 70,
+    },
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
-  
+
+export default config;
